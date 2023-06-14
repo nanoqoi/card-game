98a7655f74807f14542d66ba1ee9767509890c62
Synqat <ken@slinky.link>
2023-06-13 20:45:15 -0600

    ez

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: '/card-game/',
+  root: '.',
+  base: './',
   plugins: [tsconfigPaths()],
   build: {
     outDir: 'docs',
